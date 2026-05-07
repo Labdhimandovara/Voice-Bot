997a05bd82052d2526f8b5152de799a9d75a7678
Labdhi Mandovara <mandowaralabdhi@gmail.com>
2026-05-07 21:18:42 +0530

    app.py

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ def clear_logs():
     return jsonify({"status": "cleared"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
